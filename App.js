c17033a893e566f7f7bd25cba87202c45390d1ae
Pratanu Santra <pratanusantra0098@gmail.com>
2024-04-26 18:12:12 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const toggleBtn = document.getElementById("toggle-btn");
+//   const sidebarNav = document.querySelector(".sidebar-nav");
+
+//   toggleBtn.addEventListener("click", function () {
+//     sidebarNav.classList.toggle("collapsed");
+//   });
+// });
